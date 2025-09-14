62aea3ae692faf8f8b78f27eb8996ef4b8563b52
4d3Lx4d0 <129032805+4d3Lx4d0@users.noreply.github.com>
2025-09-15 01:19:47 +0700

    Game Over Screen

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
-
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
     float time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("UI Game Over")]
+    public GameObject gameOverPanel; // Panel UI untuk Game Over
+    public Text scoreText;
     void Start()
     {
 
@@ -25,9 +29,31 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
         return time;
     }
 
-    public void GameOver()
+        public void GameOver()
     {
-        // Implement game over logic here
+
         Debug.Log("Game Over!");
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(true);
+        }
+
+        int finalScore = (int)time;
+        if (scoreText != null)
+        {
+            scoreText.text = finalScore.ToString() + " POINTS";
+        }
+    }
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
+    
+
